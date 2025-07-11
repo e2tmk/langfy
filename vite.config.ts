146b11eec188658f5e5871dc4884e5238c459090
zdearo <adryelzd77@outlook.com>
2025-07-11 15:11:01 -0300

    chore(assets): remove compiled CSS file from repository
    delete `app-Ds5Oa3ma.css`, an autogenerated file in `resources/dist/assets`, to maintain a clean repository and avoid committing build artifacts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -10,5 +11,15 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]'
+            }
+        }
+    }
 })
